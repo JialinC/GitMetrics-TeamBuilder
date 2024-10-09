7bc183ef3c0ae72921e1afc3f9d3fdbce2012930
JialinC <cjl742952519@gmail.com>
2024-10-09 14:09:58 -0400

    add miner for collect github metrics

--- app/metrics_miner/user_metrics_miner.py
+++ app/metrics_miner/user_metrics_miner.py
@@ -1,21 +1,18 @@
-# Standard library imports
+"""
+This module provides the `UserMetricsMiner` class, which is responsible for collecting and processing GitHub metric 
+data for a user. The class utilizes various GitHub GraphQL queries to fetch user profile statistics, repositories,
+and contributions over a specified time range.
+"""
+
+import logging
 from datetime import datetime
 from collections import Counter
-
-# Third-party imports s
-# import pandas as pd
-
-# Local application imports
+from typing import List, Dict, Any, Set, Tuple
+import numpy as np
 import app.github_query.utils.helper as helper
-from app.github_query.github_graphql.client import Client, QueryFailedException
-
-# User profiles queries
+from app.github_query.github_graphql.client import QueryFailedException
 from app.github_query.queries.profiles import UserProfileStats
-
-# User contributions queries
-from app.github_query.queries.contributions import UserRepositories
-
-# Time range contributions queries
+from app.github_query.queries.repositories import UserRepositories
 from app.github_query.queries.time_range_contributions import (
     UserContributionsCollection,
 )
@@ -23,15 +20,17 @@ from app.github_query.queries.time_range_contributions import (
 
 class UserMetricsMiner:
     """
-    Helps mining user GitHub metrics data.
+    Class to collect GitHub metric data for a user.
     """
 
-    def __init__(self, client: Client):
+    def __init__(self, client):
         self._client = client
-        self.exceptions = []
         self.profile_stats = {}
+        self.user_owned_repo = []
+        self.commits = {}
+        self.exceptions = []
 
-    def fetch_user_profile_stats(self, login):
+    def fetch_user_profile_stats(self, login: str) -> None:
         """
         Fetch user profile stats.
 
@@ -39,166 +38,184 @@ class UserMetricsMiner:
             login (str): The user login.
 
         Returns:
-            Counter: A counter of user contributions.
+            None
         """
         try:
-            response = self._client.execute(
-                query=UserProfileStats(), substitutions={"user": login}
-            )
-            profile_stats = UserProfileStats.profile_stats(response)
-            setattr(self, "profile_stats", profile_stats)
-        except QueryFailedException:
+            response = self._client.execute(query=UserProfileStats(login=login))
+            self.profile_stats = UserProfileStats.profile_stats(response)
+        except QueryFailedException as e:
+            logging.error("Query failed for user %s: %s", login, e)
             self.exceptions.append(login + " DNE")
-            return
+            self.profile_stats = None
 
-    def fetch_user_contributions(self, login, start, end):
+    def fetch_user_contributions(self, login: str, start: str, end: str) -> Counter:
         """
         Fetch user contributions between start and end dates.
 
         Args:
             login (str): The user login.
-            start (datetime): The start date.
-            end (datetime): The end date.
+            start (str): The start date.
+            end (str): The end date.
 
         Returns:
-            Counter: A counter of user contributions.
+            None
         """
         contributions = Counter({"res_con": 0, "commit": 0, "pr_review": 0})
         period_end = helper.add_by_days(start, 365)
 
-        while start < end:
-            if period_end > end:
-                period_end = end
-            response = self._client.execute(
-                query=UserContributionsCollection(),
-                substitutions={"user": login, "start": start, "end": period_end},
-            )
-            queried_contribution = (
-                UserContributionsCollection.user_contributions_collection(response)
-            )
-            for key in contributions:
-                contributions[key] += queried_contribution[key]
-            start = period_end
-            period_end = helper.add_by_days(start, 365)
+        try:
+            while start < end:
+                if period_end > end:
+                    period_end = end
+                response = self._client.execute(
+                    query=UserContributionsCollection(
+                        login=login, start=f'"{start}"', end=f'"{period_end}"'
+                    )
+                )
+                queried_contribution = (
+                    UserContributionsCollection.user_contributions_collection(response)
+                )
+                for key in contributions:
+                    contributions[key] += queried_contribution[key]
+                start = period_end
+                period_end = helper.add_by_days(start, 365)
+            self.commits = contributions
+        except QueryFailedException as e:
+            logging.error("Error fetching contributions for user %s: %s", login, e)
+            self.exceptions.append(f"Error fetching contributions for {login}: {e}")
+            self.commits = Counter({"res_con": 0, "commit": 0, "pr_review": 0})
 
-        return contributions
-
-    def fetch_user_repositories(self, attribute_name, login, is_fork, ownership):
+    def fetch_user_repositories(self, login: str):
         """
-        Helper method to fetch user repositories and handle exceptions.
-
+        Fetch user repositories based on the given criteria.
         Args:
-            attribute_name (str): The name of the attribute to store the results.
             login (str): The user login to substitute in the query.
-            is_fork (bool): Whether the repository is a fork.
-            ownership (str): The ownership type of the repository.
+
+        Returns:
+            None
         """
         try:
             repositories = []
-            for response in self._client.execute(
-                query=UserRepositories(),
-                substitutions={
-                    "user": login,
-                    "pg_size": 100,
-                    "is_fork": is_fork,
-                    "ownership": ownership,
-                    "order_by": {"field": "CREATED_AT", "direction": "ASC"},
-                },
-            ):
+            for response in self._client.execute(query=UserRepositories(login=login)):
                 repositories += UserRepositories.user_repositories(response)
-            setattr(self, attribute_name, repositories)
-        except QueryFailedException:
-            self.exceptions.append(f"{login} {attribute_name}")
-            return
+            self.user_owned_repo = repositories
+        except QueryFailedException as e:
+            logging.error("Error fetching owned repo for user %s: %s", login, e)
+            self.exceptions.append(f"Error fetching owned repo for {login}: {e}")
+            self.user_owned_repo = []
 
-    def mine(self, login: str):
+    def calculate_lifetime(self, created_at: str) -> int:
         """
-        Collect GitHub metric data for a user.
+        Calculate the GitHub account life span.
         Args:
-            login: user GitHub account
+            created_at: The creation date of the GitHub account.
+        Returns:
+            The lifespan in days.
         """
-        # For this specific purpose, we can straightly use UserProfileStats query.
-        # Beacuse we only care user contributions till now instead of a specific time range.
-        # UserProfileStats query is enough to provide all the necessary information and it is faster.
+        try:
+            gh_start = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
+            gh_end = datetime.now()
+            return (gh_end - gh_start).days
+        except ValueError as e:
+            logging.error("Error parsing date %s: %s", created_at, e)
+            return 0
 
+    def calculate_language_statistics(self, langs: Set[str]):
+        """
+        Calculate language statistics for the user's repositories.
+
+        Args:
+            langs (Set[str]): A set of languages to calculate statistics for.
+
+        Returns:
+            Tuple[int, int]: A tuple containing the number of unique languages and the total size of popular languages.
+        """
+        user_langs = set()
+        lang_size = self.calculate_repo_language_size(
+            self.user_owned_repo, langs, user_langs
+        )
+        return len(user_langs), lang_size
+
+    def calculate_repo_language_size(
+        self, repos: List[Dict[str, Any]], langs: Set[str], lang_set: Set[str]
+    ) -> int:
+        """
+        Calculate the size of languages in a repository.
+        Args:
+            repos: The list of repositories.
+            pop_lang: The set of popular languages.
+            l_set: The set to store all languages.
+        Returns:
+            The total size of popular languages.
+        """
+        size = 0
+        for repo in repos:
+            languages = repo["languages"]
+            for language in languages["edges"]:
+                lang = language["node"]["name"]
+                lsize = language["size"]
+                if lang in langs:
+                    size += lsize
+                lang_set.add(lang)
+        return size
+
+    def fetch_user_contributions_data(
+        self, login: str, created_at: str
+    ) -> Tuple[int, int, int, int, int]:
+        """
+        Fetch user contributions data.
+        Args:
+            login (str): user GitHub account
+            created_at (str): The creation date of the GitHub account.
+        Returns:
+            A tuple containing commits, comments, pull requests, issues, and repositories count.
+        """
+        gh_start = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
+        gh_end = datetime.now()
+        end = gh_end.strftime("%Y-%m-%dT%H:%M:%SZ")
+        start = gh_start.strftime("%Y-%m-%dT%H:%M:%SZ")
+        self.fetch_user_contributions(login, start, end)
+        commits = self.commits["commit"]
+        comments = (
+            self.profile_stats["commit_comments"]
+            + self.profile_stats["issue_comments"]
+            + self.profile_stats["gist_comments"]
+            + self.profile_stats["repository_discussion_comments"]
+        )
+        pr = self.commits["pr_review"] + self.profile_stats["pull_requests"]
+        issues = self.profile_stats["issues"]
+        repoc = self.profile_stats["repositories"]
+        return commits, comments, pr, issues, repoc
+
+    def mine(self, login: str, pop_lang: Set[str]):
+        """
+        Mines user metrics by fetching and calculating various statistics.
+
+        Args:
+            login (str): The user's login.
+            pop_lang (Set): The list of interested languages.
+
+        Returns:
+            list: A list of user metrics.
+        """
         self.fetch_user_profile_stats(login)
         created_at = self.profile_stats["created_at"]
-        print(created_at)
-        # B = commitComments + issueComments + gistComments + repositoryDiscussionComments + repositoryDiscussions
-        # C = PullRequestReviewContributions + pullRequests
-        # D = Issues
 
-        # lifespan bc['lifespan'] = pre_class_metrics['lifeSpan']
-        now = datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ")
-        gh_start = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%SZ")
-        print(gh_start)
-        gh_end = datetime.now()  # datetime.strptime(now, "%Y-%m-%dT%H:%M:%SZ")
-        print(gh_end)
-        # Calculate the GtiHub account life span (lifespan)
-        lifetime = gh_end - gh_start
-        print(lifetime)
+        lifetime = self.calculate_lifetime(created_at)
+        self.fetch_user_repositories(login)
 
-        # Fetch user repos using the helper method.
-        # This still need to be fetched separately because general profile stats do not provide this info.
-        # E = A lang numbers + C langs numbers + B lang numbers + D langs numbers bc['E']
-        # F = A lang size + C lang size + B lang size + D lang size= bc['F']
-        # G = ABCD_count = ['repoACount', 'repoBCount', 'repoCCount', 'repoDCount']
-        # self.fetch_user_repositories("repo_A", login, False, "OWNER")
-        # self.fetch_user_repositories("repo_B", login, True, "OWNER")
-        # self.fetch_user_repositories("repo_C", login, False, "COLLABORATOR")
-        # self.fetch_user_repositories("repo_D", login, True, "COLLABORATOR")
+        l_count, pop_lang_size = self.calculate_language_statistics(pop_lang)
+        commits, comments, pr, issues, repoc = self.fetch_user_contributions_data(
+            login, created_at
+        )
 
-        # A = RestrictedContributionsCount + CommitContributionsbc
-        # Fetch user number of private contributions and commits.
-        # This can be done with time range contributions query.
-        # Main logic
-        # end = gh_end
-        # start = gh_start
-        # cumulated_contributions_collection = self.fetch_user_contributions(
-        #     login, start, end
-        # )
-        # print(cumulated_contributions_collection)
-
-    # def fetch_contributions(self, query_class, method_name, attribute_name, login):
-    #     """
-    #     Helper method to fetch various types of contributions and handle exceptions.
-    #     When only need counts, this can be used to fetch all kinds of contributions.
-    #     For contribution specific information, use the corresponding fetch method.
-
-    #     Args:
-    #         query_class (type): The class of the query to execute.
-    #         method_name (str): The name of the method to call on the query class.
-    #         attribute_name (str): The name of the attribute to store the results.
-    #         login (str): The user login to substitute in the query.
-    #     """
-    #     try:
-    #         setattr(self, attribute_name, [])
-    #         for response in self._client.execute(
-    #             query=query_class(), substitutions={"user": login, "pg_size": 100}
-    #         ):
-    #             method = getattr(query_class, method_name)
-    #             getattr(self, attribute_name).extend(method(response))
-    #     except QueryFailedException:
-    #         self.exceptions.append(f"{login} {attribute_name}")
-    #         return
-
-    # def fetch_comments(self, query_class, method_name, attribute_name, login):
-    #     """
-    #     Helper method to fetch comments and handle exceptions.
-
-    #     Args:
-    #         query_class (type): The class of the query to execute.
-    #         method_name (str): The name of the method to call on the query class.
-    #         attribute_name (str): The name of the attribute to store the results.
-    #         login (str): The user login to substitute in the query.
-    #     """
-    #     try:
-    #         setattr(self, attribute_name, [])
-    #         for response in self._client.execute(
-    #             query=query_class(), substitutions={"user": login, "pg_size": 100}
-    #         ):
-    #             method = getattr(query_class, method_name)
-    #             getattr(self, attribute_name).extend(method(response))
-    #     except QueryFailedException:
-    #         self.exceptions.append(f"{login} {attribute_name}")
-    #         return
+        return [
+            login,
+            lifetime,
+            commits,
+            comments,
+            pr + issues,
+            l_count,
+            np.log10(pop_lang_size + 1),
+            repoc,
+        ]
