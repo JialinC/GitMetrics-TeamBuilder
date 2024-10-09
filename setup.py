09b9b0bfa785fe379030ec71c5dba1072a9a978d
JialinC <cjl742952519@gmail.com>
2024-10-09 15:01:56 -0400

    add setup.py file for the project

--- setup.py
+++ setup.py
@@ -3,11 +3,33 @@ It is used by pip to install your project."""
 
 from setuptools import setup, find_packages
 
+
+# Function to read the requirements.txt file
+def read_requirements():
+    with open("requirements.txt", encoding="utf-8") as req_file:
+        return req_file.read().splitlines()
+
+
 setup(
     name="GitMetrics-TeamBuilder",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(),
-    install_requires=[
-        "requests",  # Add other dependencies here
+    install_requires=read_requirements(),
+    entry_points={
+        "console_scripts": [
+            "team=app.driver:main",
+        ],
+    },
+    author="Jialin Cui",
+    author_email="jcui9@ncsu.edu",
+    description="A command-line tool for mining user metrics and forming teams.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/JialinC/GitMetrics-TeamBuilder",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    python_requires=">=3.6",
 )
