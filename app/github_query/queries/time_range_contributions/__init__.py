4fa728f504b1e5932e019e1f6dc0d76943161bb4
JialinC <cjl742952519@gmail.com>
2024-10-16 01:22:15 -0400

    add moudle doc string

--- app/github_query/queries/time_range_contributions/__init__.py
+++ app/github_query/queries/time_range_contributions/__init__.py
@@ -1,3 +1,9 @@
+"""
+This module initializes the time_range_contribution package by importing the
+UserContributionsCollection class, which is used to collect user contributions
+within a specified time range.
+"""
+
 from .user_contributions_collection import UserContributionsCollection
 
 __all__ = [
